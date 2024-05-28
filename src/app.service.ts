d63007665d358f3371197ab69cd04b6baefc2694
Vadim <fw.mbagg@gmail.com>
2024-05-28 07:04:03 +0300

    add readyz and healthyz endpoints

---
+++ src/app.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  readyz(): string {
+    return 'Service is ready';
+  }
+
+  healthyz(): string {
+    return 'Service alive';
+  }
+}
