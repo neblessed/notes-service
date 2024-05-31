7d1c19446f2225befefe2d18b4ae54f8583d12f9
Vadim <fw.mbagg@gmail.com>
2024-05-31 07:14:28 +0300

    add global prefix

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { AppModule } from './app.module';
 
 config();
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.SERVICE_PORT);
+    const app = await NestFactory.create(AppModule);
+    app.setGlobalPrefix('api');
+    await app.listen(process.env.SERVICE_PORT);
 }
 bootstrap();
