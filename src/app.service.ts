858da9c27788f8a7665bf3b4f190e32eaeec5359
Vadim <fw.mbagg@gmail.com>
2024-08-05 19:40:09 +0300

    update service

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
     getHello(): string {
-        return 'Hello! This is notes service!';
+        return 'Hello! This is notes service!!';
     }
 
     readyz(): string {
